d1857a36323f04c09bb40f1000f8c09d41c7e366
yuan feng <fengyuan1216@gmail.com>
2018-10-17 18:16:53 -0400

    no message

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<string>
+#include<cstring>
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<map>
+
+using namespace std;
+
+int main() {
+    cout<<"lala"<<endl;
+
+    for (int i = 5; i < 6; i++) {
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
