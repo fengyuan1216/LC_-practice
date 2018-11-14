25ed2a662d8fe60b3c23f047716189451ca6d8a7
yuan feng <fengyuan1216@gmail.com>
2018-11-13 21:59:57 -0500

    no message

--- practice.cpp
+++ practice.cpp
@@ -8,6 +8,17 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int xxx) {
+        val = xxx;
+        left = NULL;
+        right = NULL;
+    }
+};
+
 
 int Longest_Arithmetic_Progression() {
     bool brute_force = false;
@@ -78,11 +89,115 @@ int Longest_Arithmetic_Progression() {
 }
 
 
+TreeNode* buildBST_helper(vector<int>& nums, int begin, int end) {
+    if (begin == end) {
+        TreeNode* newnode = new TreeNode(nums[begin]);
+        return newnode;
+    }
+    if (begin > end)
+        return NULL;
+
+    int mid = begin + (end - begin) / 2;
+    TreeNode* newnode = new TreeNode(nums[mid]);
+    
+    newnode->left = buildBST_helper(nums, begin, mid-1);
+    newnode->right = buildBST_helper(nums, mid+1, end);
+    return newnode;
+}
+
+
+TreeNode* buildBST(vector<int>& nums) {
+    TreeNode* root = buildBST_helper(nums, 0, nums.size()-1);
+    return root;
+}
+
+
+void bst_inorder(TreeNode* root) {
+    vector<int> ans;
+    stack<TreeNode*> s;
+    TreeNode* p = root;
+
+    while (true) {
+        if (p != NULL) {
+            s.push(p);
+            p = p->left;
+        }
+        else {
+            if (s.empty())
+                break;
+            p = s.top();
+            ans.push_back(p->val);
+            s.pop();
+            p = p->right;
+        }
+    }
+
+    for (int i = 0; i < ans.size(); i++)
+        cout<<ans[i]<<endl;
+}
+
+
+void bst_find_next_helper(TreeNode* p, int target, vector<TreeNode*> onepath, vector<TreeNode*>& path) {
+    if (p == NULL)
+        return;
+    onepath.push_back(p);
+    if (p->val == target) {
+        path = onepath;
+        return;
+    }
+    bst_find_next_helper(p->left, target, onepath, path);
+    bst_find_next_helper(p->right, target, onepath, path);
+    return;   
+}
+
+void bst_find_next(TreeNode* root, int target) {
+    vector<TreeNode*> onepath;
+    vector<TreeNode*> path;
+    bst_find_next_helper(root, target, onepath, path);
+
+    TreeNode* point = NULL;
+    if (path[path.size()-1]->right)
+        point = path[path.size()-1]->right;
+    else {
+        for (int i = path.size()-2; i >= 0; i--) {
+            if (path[i]->left == path[i+1]) {
+                point = path[i]->right;
+                break;
+            }
+        }
+    }
+
+    if (point == NULL)
+        cout<<"no next point"<<endl;
+    else {
+        while (point->left)
+            point = point->left;
+        cout<<point->val<<endl;
+    }
+
+}
+
+
+void bst_test() {
+    vector<int> nums;
+    int n = 11;
+    for (int i = 0; i < n; i++)
+        nums.push_back(i);
+
+    TreeNode* root = buildBST(nums);
+    // bst_inorder(root);
+    for (int i = 0; i < n; i++)
+        bst_find_next(root, i);
+}
+
+
+
+
+
 
 int main() {
-    int test_result1 = Longest_Arithmetic_Progression();
-
-    cout<<test_result1<<endl;
-
+    // int test_result1 = Longest_Arithmetic_Progression();
+    // cout<<test_result1<<endl;
+    bst_test();
     return 0;
 }
